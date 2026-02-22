326d5adf280b732fb711af1d20d4de1673967ef5
Romeo Sarkar <romeosarkar10x@gmail.com>
2026-02-22 22:38:00 +0530

    Removed useless css; Changed rootElem injection logic

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+
 import "./index.css";
 import App from "./App.tsx";
 
-createRoot(document.getElementById("root")!).render(
+const rootElem = document.createElement("div");
+rootElem.id = "react-root";
+
+document.body.appendChild(rootElem);
+
+createRoot(rootElem).render(
     <StrictMode>
         <App />
     </StrictMode>,
